a5affecba4e74beff7a5d83fa0867de92ce60ac2
Edelweiss35 <bluesky_butterfly@outlook.com>
2019-02-10 16:14:42 -0800

    make it better

--- twitter/templates/js/twengage.js
+++ twitter/templates/js/twengage.js
@@ -3,8 +3,8 @@ jQuery('.faq-acc .btn-info').addClass('collapsed');
   jQuery('.faq-acc .btn-info').click(function(){
     jQuery(this).parent().toggleClass('shadow-acc');
   });
-  jQuery('.tg-ul li').click(function(){
-    jQuery('.tg-ul li').removeClass('active');
+  jQuery('.tg-ul div').click(function(){
+    jQuery('.tg-ul div').removeClass('active');
     jQuery(this).addClass('active');
   });
 });
